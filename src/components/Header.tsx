25bfaef41ab9471a709e0707ebc9faa34eb269ae
omokgosu <anfakt0606@naver.com>
2025-10-10 15:43:23 +0900

    [ feat ] style transition 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,44 +8,44 @@ export default function Header() {
       </h1>
       <div className="flex items-center gap-2 flex-1 order-1">
         <Image src="/icons/cursor_logo.svg" alt="logo" width={16} height={16} />
-        <ul className="flex items-center gap-2">
+        <ul className="flex font-medium items-center gap-2">
           <li>
-            <button className="px-2 py-1 text-sm hover:bg-muted/30 transition-colors rounded">
+            <button className="px-2 py-1 text-sm hover:bg-muted/30 rounded">
               File
             </button>
           </li>
           <li>
-            <button className="px-2 py-1 text-sm hover:bg-muted/30 transition-colors rounded">
+            <button className="px-2 py-1 text-sm hover:bg-muted/30 rounded">
               Edit
             </button>
           </li>
           <li>
-            <button className="px-2 py-1 text-sm hover:bg-muted/30 transition-colors rounded">
+            <button className="px-2 py-1 text-sm hover:bg-muted/30 rounded">
               Selection
             </button>
           </li>
           <li>
-            <button className="px-2 py-1 text-sm hover:bg-muted/30 transition-colors rounded">
+            <button className="px-2 py-1 text-sm hover:bg-muted/30 rounded">
               View
             </button>
           </li>
           <li>
-            <button className="px-2 py-1 text-sm hover:bg-muted/30 transition-colors rounded">
+            <button className="px-2 py-1 text-sm hover:bg-muted/30 rounded">
               Go
             </button>
           </li>
           <li>
-            <button className="px-2 py-1 text-sm hover:bg-muted/30 transition-colors rounded">
+            <button className="px-2 py-1 text-sm hover:bg-muted/30 rounded">
               Run
             </button>
           </li>
           <li>
-            <button className="px-2 py-1 text-sm hover:bg-muted/30 transition-colors rounded">
+            <button className="px-2 py-1 text-sm hover:bg-muted/30 rounded">
               Terminal
             </button>
           </li>
           <li>
-            <button className="px-2 py-1 text-sm hover:bg-muted/30 transition-colors rounded">
+            <button className="px-2 py-1 text-sm hover:bg-muted/30 rounded">
               Help
             </button>
           </li>
